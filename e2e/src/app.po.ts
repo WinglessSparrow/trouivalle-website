6c62ec11e1e2a4c17122a34eee5fbb9ad8b5862f
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2022-01-27 15:29:30 +0100

    restructuring the Project and e2e added

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo(destination: string) {
+    return browser.get(destination);
+  }
+
+  getParagraphText() {
+    return element(by.deepCss('app-root ion-content')).getText();
+  }
+}
