aab6800d74a45e9a48e63c2389eca34a98829000
Uwe <Uwe@>
2022-04-01 17:17:51 +0200

    anfang validation

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validator} from "@angular/forms";
+import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 import {MatFormField} from "@angular/material/form-field";
 import {faEnvelope, faBox, faBoxesStacked} from "@fortawesome/free-solid-svg-icons";
-import {F} from "@angular/cdk/keycodes";
 
 
 @Component({
@@ -17,25 +16,101 @@ export class OrderComponent implements OnInit {
     faBoxesStacked = faBoxesStacked;
 
     sender = new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl(),
-        email: new FormControl(),
-        city: new FormControl(),
-        postalCode: new FormControl(),
-        street: new FormControl(),
-        houseNumber: new FormControl(),
-        country: new FormControl()
+        firstName: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        lastName: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        email: new FormControl("", [
+            Validators.required,
+            Validators.email,
+        ]),
+        city: new FormControl("", [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(20),
+                Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+            ]),
+        postalCode: new FormControl("", [
+            Validators.required,
+            Validators.maxLength(5),
+            Validators.minLength(5),
+            Validators.pattern("[0-9]{5}")
+        ]),
+        street: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        houseNumber: new FormControl("", [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(4),
+            Validators.pattern("[0-9]*")
+        ]),
+        country: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ])
     });
 
     receiver = new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl(),
-        email: new FormControl(),
-        city: new FormControl(),
-        postalCode: new FormControl(),
-        street: new FormControl(),
-        houseNumber: new FormControl(),
-        country: new FormControl()
+        firstName: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        lastName: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        email: new FormControl("", [
+            Validators.required,
+            Validators.email
+        ]),
+        city: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        postalCode: new FormControl("", [
+            Validators.required,
+            Validators.maxLength(5),
+            Validators.minLength(5),
+            Validators.pattern("[0-9]{5}")
+        ]),
+        street: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ]),
+        houseNumber: new FormControl("", [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(4),
+            Validators.pattern("[0-9]*")
+        ]),
+        country: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(20),
+            Validators.pattern("[a-zA-ZäöüÄÖÜß]*")
+        ])
     });
 
     dimensions = new FormGroup({
